bdb490630c3dca34d2eb07c76ac6e7001d239496
true_mogician <925269713@qq.com>
2022-03-24 15:13:08 +0800

    feat: tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,59 @@
 <template>
-	<div id="news" class="ui grid" :class="gridColumn" v-if="ready">
+	<div id="news-container">
+		<div id="tabular" class="ui top attached tabular menu">
+			<div class="active item">智能推荐</div>
+			<div class="item">政治</div>
+			<div class="item">经济</div>
+			<div class="item">科技</div>
+			<div class="item">体育</div>
+			<div class="item">娱乐</div>
+			<div class="item">不同视角</div>
+		</div>
 		<div
-			class="column"
-			style="padding:1em"
-			v-for="info in newsInfos"
-			:key="info.id"
+			class="ui bottom attached active tab segment"
+			style="background-color:transparent"
 		>
-			<NewsCard
-				style="margin:0 auto"
-				:id="info.id"
-				:title="info.title"
-				:url="info.url"
-				:image="info.image"
-				:date="info.date"
-				:source="info.source"
-			/>
+			<div id="news" class="ui grid" :class="gridColumn" v-if="ready">
+				<div
+					class="column"
+					style="padding:1em"
+					v-for="info in newsInfos"
+					:key="info.id"
+				>
+					<NewsCard
+						style="margin:0 auto"
+						:id="info.id"
+						:title="info.title"
+						:url="info.url"
+						:image="info.image"
+						:date="info.date"
+						:source="info.source"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <style>
+#news-container {
+	background-image: url("../assets/image/background/default.jpg");
+}
+#tabular {
+	justify-content: center;
+	padding-top: 10px;
+}
+#tabular > .item {
+	color: white;
+	cursor: pointer;
+}
+#tabular > .active.item {
+	color: black;
+}
 #news {
 	height: 100%;
 	margin: 0;
 	padding: 2em 3em;
-	background-image: url("../assets/image/background/default.jpg");
 }
 </style>
 
@@ -32,9 +61,10 @@
 import "basic-type-extensions";
 import Axios, { AxiosError } from "axios";
 import { Options, Vue } from "vue-class-component";
-import { News } from "news-recommendation-entity";
 import NewsCard from "../components/NewsCard.vue";
 import ErrorHandler from "../error-handler";
+
+import type { News } from "news-recommendation-entity";
 
 const translator = ["zero", "one", "two", "three", "four", "five", "six"];
 class Props {
@@ -67,7 +97,10 @@ export default class HomeView extends Vue.with(Props) {
 		if ((document as any).global.pending === true)
 			await Promise.wait(() => !(document as any).global.pending, 100);
 		Axios.get("/api/news/recommendation", {
-			params: { count: this.count ?? 10 },
+			params: {
+				count: this.count ?? 12,
+				random: true
+			},
 		}).then(
 			response => {
 				Axios.get("/api/news/infos", {
